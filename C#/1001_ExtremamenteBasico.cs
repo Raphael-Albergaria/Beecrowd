3a882196f6db5eedc4c9b5a159d849f2e0cea311
Raphael-Albergaria <hraphael.albergaria@hotmail.com>
2022-12-14 16:30:48 -0300

    Exercício 1001

---
+++ C#/1001_ExtremamenteBasico.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtremamenteBasico_1001
+{
+    public class Program
+    {
+        public static void Main()
+        {git b
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int X = A + B;
+
+            Console.WriteLine($"X = {X}");
+        }
+    }
+}
